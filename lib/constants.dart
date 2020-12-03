3390cc08fa85a4718ca4e74bef2b6ebf27f671ff
Helena Brito <helena_b_dias@hotmail.com>
2020-12-03 12:31:05 -0300

    Welcome Screen

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFB23B3F);
+const kPrimaryLightColor = Color(0xFFF5D2D1);
---
+++ lib/screens/welcome/components/body.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'package:signup_page_App/components/rounded_button.dart';
+import 'package:signup_page_App/constants.dart';
+import 'package:signup_page_App/screens/welcome/components/background.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // This size provide us total height and width of our screen
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'WELCOME TO EDU',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            Image.asset(
+              'assets/images/chat2.jpg',
+              height: size.height * 0.45,
+            ),
+            SizedBox(
+              height: size.height * 0.01,
+            ),
+            RoundedButton(
+              text: 'LOGIN',
+              press: () {},
+            ),
+            RoundedButton(
+              text: 'SIGNUP',
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+              press: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
