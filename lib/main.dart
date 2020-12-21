7cf018fddcd04c3d3be9a3944286c2128aa3b2c1
Helena Brito <helena_b_dias@hotmail.com>
2020-12-21 10:36:28 -0300

    Add README and Upgrade Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Raleway',
       ),
       home: WelcomeScreen(),
     );
